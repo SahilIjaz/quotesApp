9bbabdd6989261db3afbf32c06ac5562365e1757
SahilIjaz <hssahil2913@gmail.com>
2024-08-09 16:49:08 +0500

    deployment commit controller changed

--- controllers/quotesControllers.js
+++ controllers/quotesControllers.js
@@ -93,7 +93,7 @@ exports.remainder=catchAsync(async(req,res,next)=>{
     const remainders=await quote.find({
         user:req.user._id
     })
-if(!remainders||remainders.time==' '||remainders.days==' ')
+if(!remainders)
 {
     return next(new appError('No remainders to be shown!',404))
 }
